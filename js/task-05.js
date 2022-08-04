0c37ac1f41d5cbcf6da0d7ce136b621b41338d4a
Vitaliy Ivanov <dotaivanov@gmail.com>
2022-08-04 17:47:53 +0300

    fixed task 5, task 10
    task 5 - added checking for input length
    task 10 - add html elements not in the loop

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,14 @@ const textInput = document.querySelector('#name-input')
 const textOutput = document.querySelector('#name-output')
 
 textInput.addEventListener('input', onInputChange) 
+textInput.addEventListener('blur', event => {
+    if(event.currentTarget.value.length === 0) {
+        textOutput.textContent = 'Anonymous'
+    }
+})
 
 function onInputChange (event) {
     textOutput.textContent = event.currentTarget.value
 }
+
+
--- js/task-10.js
+++ js/task-10.js
@@ -10,14 +10,17 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+
 function createBoxes(amount) {
+  const boxesArr = []
   for(let i = 0; i < amount; i += 1) {
     const div = document.createElement('div')
-    boxes.appendChild(div)
     div.style.width = 30 + i * 10 +'px'
     div.style.height = 30 + i * 10 + 'px'
     div.style.backgroundColor = getRandomHexColor()
+    boxesArr.push(div)
   }
+  boxesArr.forEach(el => boxes.appendChild(el))
 }
 
 function destroyBoxes() {
